916fec33c2c9b29ec278fa07ccc340a4a0eaf4cb
MariaTrunk <75212906+MariaTrunk@users.noreply.github.com>
2023-01-22 17:56:22 +0300

    Part 1
    Добавили текстуру фона

---
+++ Fly/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fly
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static Random r = new Random();
+        double x = 100;
+        double y = 100;
+        double curX = 100;
+        double curY = 100;
+        //private double speedX = 200;
+        // private double speedY = 300;
+        private double speed = 200;
+        int count = 0;  
+        
+
+        private TimeSpan prevTime;
+        private Stopwatch stopwatch = Stopwatch.StartNew();
+       
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+           
+                prevTime = stopwatch.Elapsed;
+                CompositionTarget.Rendering += CompositionTarget_Rendering;
+            
+           
+        }
+
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            var currentTime = stopwatch.Elapsed;
+            var delta = (currentTime - prevTime).TotalSeconds;
+            prevTime = currentTime;
+
+            lblInfo.Content = delta;
+
+            curX = curX + speed * delta;
+            curY = curY + speed * delta;
+
+            if (curX != x && curY != y)
+            {
+                if (curX < x)
+                {
+                    speed = -speed;
+                    curX = x;
+
+                }
+                else if (curX > x)
+                {
+                    speed = -speed;
+                    curX = x;
+
+                }
+                if (curY < y)
+                {
+                    speed = -speed;
+                    curY = y;
+
+                }
+                else if (curY > y)
+                {
+                    speed = -speed;
+                    curY = y;
+
+                }
+                
+            }
+
+            
+            Canvas.SetTop(fly, curY);
+            Canvas.SetLeft(fly, curX);
+
+        }
+
+        private void fly_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.Source is Image)
+            {
+                x = Convert.ToDouble(r.Next((int)MyCanvas.ActualWidth - (int)fly.Width) );
+                y = Convert.ToDouble(r.Next((int)MyCanvas.ActualHeight - (int)fly.Height) );
+
+               
+                count ++;
+                lblScore.Content = count;
+                
+                
+            }
+        }
+    }
+}
---
+++ ScrollingPlatformGame/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScrollingPlatformGame
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/bg.jpeg"));
+
+            bg1.Fill=imageBrush;
+        }
+
+        private void MapCanvas_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void MapCanvas_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
